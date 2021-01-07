59648c9a82c544ac73e95bda70ff8e42d11bd539
Ryan Hageman <ryan@rhageman.com>
2021-01-07 15:13:10 -0600

    Refactor drawingPromptGenerator

--- src/components/DrawingPrompt/drawingPromptGenerator.js
+++ src/components/DrawingPrompt/drawingPromptGenerator.js
@@ -1,29 +1,37 @@
 import PROMPT_DATA from "./drawingPromptData"
 
 export default class DrawingPromptGenerator {
-  prompt() {
-    const { travel } = PROMPT_DATA
-    // const food = this._randomEntry(eats)
-    const moves = this._randomEntry(travel)
-    const movement = this._randomEntry(['gets around', 'travels'])
-
-    return `${this._first_part()}, ${this._second_part()} and ${movement} ${moves}`
+  constructor() {
+    this.placesToLive = PROMPT_DATA.lives
+    this.possibleSizes = PROMPT_DATA.size
+    this.listOfFoods = PROMPT_DATA.eats
+    this.modesOfTransport = PROMPT_DATA.travel
+    this.extrasList = PROMPT_DATA.extra
   }
 
-  _first_part() {
-    const { lives, size } = PROMPT_DATA
-    const home = `lives ${this._randomEntry(lives)}`
-    const scale = this._randomEntry(size)
+  prompt() {
+    return `${this._scale()}, ${this._characteristic()} ${this._movement()}`
+  }
+
+  _scale() {
+    const home = `lives ${this._randomEntry(this.placesToLive)}`
+    const scale = this._randomEntry(this.possibleSizes)
 
     return `It ${this._randomEntry([home, scale])}`
   }
 
-  _second_part() {
-    const { eats, extra } = PROMPT_DATA
-    const food = `eats ${this._randomEntry(eats)}`
-    const other = this._randomEntry(extra)
+  _characteristic() {
+    const food = `eats ${this._randomEntry(this.listOfFoods)}`
+    const other = this._randomEntry(this.extrasList)
 
     return `${this._randomEntry([food, other])}`
+  }
+
+  _movement() {
+    const moves = this._randomEntry(this.modesOfTransport)
+    const movement = this._randomEntry(["gets around", "travels"])
+
+    return `and ${movement} ${moves}`
   }
 
   _randomEntry(array) {
